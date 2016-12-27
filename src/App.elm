9b39d6b3a1f8bdacc1c6d772b76b9986716e0552
Rolograaf <Rolograaf@gmail.com>
2016-12-27 15:41:25 +0100

    :sparkles: First working version, two related dropboxes

--- src/App.elm
+++ src/App.elm
@@ -2,36 +2,168 @@ module App exposing (..)
 
 import Html exposing (Html, text, div, img)
 import Html.Attributes exposing (src)
+import Dict exposing (Dict)
+import Mouse
+import Dropdown as Dropdown
+import Styles.Styles as Styles
+
+
+-- main MODEL
 
 
 type alias Model =
-    { message : String
-    , logo : String
+    { apparaat : Maybe Apparaat
+    , bouwGrootte : Maybe BouwGrootte
+    , openDropDown : OpenDropDown
+    }
+
+
+type OpenDropDown
+    = AllClosed
+    | ApparaatDropDown
+    | BouwGrootteDropDown
+
+
+initialModel : Model
+initialModel =
+    { apparaat = Nothing
+    , bouwGrootte = Nothing
+    , openDropDown = AllClosed
     }
 
 
 init : String -> ( Model, Cmd Msg )
 init path =
-    ( { message = "Your Elm App is working!", logo = path }, Cmd.none )
+    ( initialModel, Cmd.none )
+
+
+
+-- simple types so we can read the code better
+
+
+type alias Apparaat =
+    String
+
+
+type alias BouwGrootte =
+    String
+
+
+
+-- global constants / config
+
+
+allApparaten : Dict Apparaat (List BouwGrootte)
+allApparaten =
+    Dict.fromList
+        [ ( "OLU", [ "1A", "1B", "2A", "2B" ] )
+        , ( "GOLU", [ "1B", "3S" ] )
+        , ( "BPU", [ "1A", "1B", "2A", "2B" ] )
+        ]
+
+
+apparaten : List Apparaat
+apparaten =
+    Dict.keys allApparaten
+
+
+apparaatConfig : Dropdown.Config Msg
+apparaatConfig =
+    { defaultText = "-- pick a unit --"
+    , clickedMsg = Toggle ApparaatDropDown
+    , itemPickedMsg = ApparaatPicked
+    }
+
+
+bouwGrootteConfig : Dropdown.Config Msg
+bouwGrootteConfig =
+    { defaultText = "-- pick a size --"
+    , clickedMsg = Toggle BouwGrootteDropDown
+    , itemPickedMsg = BouwGroottePicked
+    }
 
 
 type Msg
-    = NoOp
+    = Toggle OpenDropDown
+    | ApparaatPicked Apparaat
+    | BouwGroottePicked BouwGrootte
+    | Blur
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        Toggle dropdown ->
+            let
+                newOpenDropDown =
+                    if model.openDropDown == dropdown then
+                        AllClosed
+                    else
+                        dropdown
+            in
+                ( { model | openDropDown = newOpenDropDown }, Cmd.none )
+
+        ApparaatPicked pickedApparaat ->
+            let
+                newBouwGrootte =
+                    if model.apparaat /= Just pickedApparaat then
+                        Nothing
+                    else
+                        model.bouwGrootte
+            in
+                ( { model
+                    | apparaat = Just pickedApparaat
+                    , bouwGrootte = newBouwGrootte
+                    , openDropDown = AllClosed
+                  }
+                , Cmd.none
+                )
+
+        BouwGroottePicked pickedBouwGrootte ->
+            ( { model
+                | bouwGrootte = Just pickedBouwGrootte
+                , openDropDown = AllClosed
+              }
+            , Cmd.none
+            )
+
+        Blur ->
+            ( { model
+                | openDropDown = AllClosed
+              }
+            , Cmd.none
+            )
 
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ img [ src model.logo ] []
-        , div [] [ text model.message ]
-        ]
+    let
+        apparaatContext =
+            { selectedItem = model.apparaat
+            , isOpen = model.openDropDown == ApparaatDropDown
+            }
+
+        bouwGrootteContext =
+            { selectedItem = model.bouwGrootte
+            , isOpen = model.openDropDown == BouwGrootteDropDown
+            }
+
+        bouwGrootten =
+            model.apparaat
+                |> Maybe.andThen (\c -> Dict.get c allApparaten)
+                |> Maybe.withDefault []
+    in
+        div [ Html.Attributes.style Styles.mainContainer ]
+            [ Dropdown.view apparaatConfig apparaatContext apparaten
+            , Dropdown.view bouwGrootteConfig bouwGrootteContext bouwGrootten
+            ]
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    case model.openDropDown of
+        AllClosed ->
+            Sub.none
+
+        _ ->
+            Mouse.clicks (always Blur)
---
+++ src/Dropdown.elm
@@ -0,0 +1,101 @@
+module Dropdown exposing (Context, Config, view)
+
+{- a Dropdown component that manages its own state -}
+
+import Html exposing (..)
+import Html.Attributes exposing (style)
+import Html.Events exposing (onWithOptions)
+import Json.Decode as Json
+import Styles.Styles as Styles
+
+
+-- MODEL
+{- Context type alias
+   this is dynamic stuff - may change in each update cylce
+   this is not managed by the dropdown, but passed in from parent
+   kind of like props (including callbacks) in react
+-}
+
+
+type alias Context =
+    { selectedItem : Maybe String
+    , isOpen : Bool
+    }
+
+
+
+{- Config is the static stuff, that won't change during life cylce
+   Like functions and message constructors
+   Also transparent, because this is also owned by parent
+-}
+
+
+type alias Config msg =
+    { defaultText : String
+    , clickedMsg : msg
+    , itemPickedMsg : String -> msg
+    }
+
+
+
+-- VIEW
+
+
+view : Config msg -> Context -> List String -> Html msg
+view config context data =
+    let
+        mainText =
+            context.selectedItem
+                |> Maybe.withDefault config.defaultText
+
+        displayStyle =
+            if context.isOpen then
+                ( "display", "block" )
+            else
+                ( "display", "none" )
+
+        mainAttr =
+            case data of
+                [] ->
+                    [ style <| Styles.dropdownDisabled ++ Styles.dropdownInput
+                    ]
+
+                _ ->
+                    [ style Styles.dropdownInput
+                    , onClick config.clickedMsg
+                    ]
+    in
+        div
+            [ style Styles.dropdownContainer ]
+            [ p
+                mainAttr
+                [ span [ style Styles.dropdownText ] [ text mainText ]
+                , span [] [ text "▾" ]
+                ]
+            , ul
+                [ style <| displayStyle :: Styles.dropdownList ]
+                (List.map (viewItem config) data)
+            ]
+
+
+viewItem : Config msg -> String -> Html msg
+viewItem config item =
+    li
+        [ style Styles.dropdownListItem
+        , onClick <| config.itemPickedMsg item
+        ]
+        [ text item ]
+
+
+
+-- helper to cancel click anywhere
+
+
+onClick : msg -> Attribute msg
+onClick message =
+    onWithOptions
+        "click"
+        { stopPropagation = True
+        , preventDefault = False
+        }
+        (Json.succeed message)
